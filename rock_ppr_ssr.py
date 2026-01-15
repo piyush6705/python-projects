50febd5a5c9f9d6e20e13f680a19187771e9dd19
piyush6705 <sharmapiyush6705@gmail.com>
2026-01-15 19:40:08 +0530

    refactor code into separate functions

--- rock_ppr_ssr.py
+++ rock_ppr_ssr.py
@@ -3,16 +3,19 @@ import random
 emojis= {"r": "🪨", "s": "✂️", "p": "📃"}
 choices =('r', 'p', 's')
 
-while True:
-    user_choice= input("Rock, paper , or scissors? (r/p/s): ").lower()
-    if user_choice not in choices:
-        print("INvalid choice")
-        continue
-
-    computer_choice=random.choice(choices)
+def get_user_choice():
+    while True:
+        user_choice= input("Rock, paper , or scissors? (r/p/s): ").lower()
+        if user_choice  in choices:
+            return user_choice
+            
+        else:
+            print("Invalid choice!")
+def display_choices(user_choice , computer_choice):
     print(f'You chose {emojis[user_choice]}')
-    print(f'computer chose {emojis[computer_choice]}')
-
+    print(f'computer chose {emojis[computer_choice]}')         
+    
+def determine_winner(user_choice,computer_choice):
     if user_choice == computer_choice:
         print('Tie!')
     elif user_choice == 'r' and computer_choice =='s':
@@ -25,7 +28,22 @@ while True:
     else:
         print("You lose")
 
-    should_continue=input('Continue? (y/n): ').lower()
-    if should_continue =="n":
-        break
+def play_game():
+    while True:
+        user_choice = get_user_choice()
+
+        computer_choice=random.choice(choices)
+        
+        display_choices(user_choice, computer_choice)
+        
+        determine_winner(user_choice, computer_choice)
+        
+
+        should_continue=input('Continue? (y/n): ').lower()
+        if should_continue =="n":
+            break
+
+
+play_game()
+
     
