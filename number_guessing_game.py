acb534aaa9b6e23a0705110352a9768ffe5a7f5a
piyush6705 <sharmapiyush6705@gmail.com>
2026-02-05 23:00:02 +0530

    i made some changes in this

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,17 +1,27 @@
 
+import random 
 
-import random
+lowest_num = 1
+heighest_num = 100
+answer= random.randint(lowest_num, heighest_num)
+attempts= 0
 
-NUM = random.randint(1 , 100)
-attempts = 0
+print(f"please guess a number between {lowest_num} and  {heighest_num}")
+attempts= 0
 while True:
-    GNUM = int(input("GUESS THE NUMBER between 1 and 100: "))
-    if(GNUM == NUM):
-        print("YOU GUESSED CORRECT NUMBER IN ",attempts," attempts")
-        break
-    if(GNUM > NUM):
-        print("YOU HAVE GUESSED THE LARGE NUMBER")
-        attempts += 1
+    guess= int(input(f"Enter your guess betwween {lowest_num} and  {heighest_num}: "))
+    if guess < lowest_num or guess > heighest_num:
+        print(f"OUT OF RANGE ! Please guess a number between {lowest_num} and  {heighest_num}")
+        continue
+    if guess < answer:
+        print("too low, try again")
+        attempts +=1
+
+    elif guess >answer:
+        print("too high , try agian")
+        attempts +=1
     else:
-        print("YOU HAVE GUESSED THE SMALLER NUMBER")
-        attempts += 1
+        print(f"congrats!! you guessed the num {answer} in {attempts} attempts")
+        break
+
+
