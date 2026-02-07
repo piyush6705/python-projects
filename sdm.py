4a4e9d0b20aebdcc44127a28e257a48dd2a9add6
piyush6705 <sharmapiyush6705@gmail.com>
2026-02-07 12:39:00 +0530

    one more project for newbie

---
+++ sdm.py
@@ -0,0 +1,67 @@
+
+student_grades ={}
+
+def add_student(name, grade):
+    student_grades[name] = grade
+
+    print(f"Added {name} with a {grade}")
+
+def update_student(name, grade):
+    if name in student_grades:
+        student_grades[name] = grade
+
+        print(f"{name} with  marks are updated {grade}")
+
+    else:
+        print(f"{name} is not found in the student grades")
+
+
+def delete_student(name):
+    if name in student_grades:
+        del student_grades[name]
+        print(f"{name} has been successfully deleted from  the student grade")
+    else:
+        print(f"{name} is not found !")
+
+
+
+def display_students():
+    if student_grades:
+        for name, grade in student_grades.items():
+            print(f"{name}: {grade}")
+    else:
+            print("No students found.")
+
+def main():
+    while True:
+        print("\nStudent Grade Management System")
+        print("1. Add Student")
+        print("2. Update Student")
+        print("3. Delete Student")
+        print("4. View Students")
+        print("5. Exit")
+
+        choice  = int(input("Enter your choice(1-5):"))
+
+        if choice == 1:
+            name = input("Enter student name: ")
+            grade = int(input("Enter student grade: "))
+            add_student(name, grade)
+        elif choice ==2:
+            name = input("Enter student name: ")
+            grade = int(input("Enter student grade: "))
+            update_student(name, grade)
+        elif choice ==3:
+            name = input("Enter student name: ")
+            delete_student(name)
+        elif choice ==4:
+            display_students()
+        elif choice ==5:
+            print("CLOSING THE PROGRAM. Goodbye!")
+            break
+        else:
+            print("Invalid choice. ")
+    
+
+if __name__ == "__main__":
+    main()
